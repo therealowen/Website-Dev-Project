63045914ff8ea5d90e68864854215a08d38b72b9
Owen <huyiqun2017@gmail.com>
2022-02-27 22:28:44 -0500

    Static Web 1.0

---
+++ Static Website/php/email_sub.php
@@ -0,0 +1,8 @@
+<?php
+    $userName = $_GET["userName"];
+    $phoneNum = $_GET["phoneNum"];
+    $emailAdd = $_GET["emailAdd"];
+
+?>
+<h1><?php echo "Dear " .$userName .", we've received your signup information, thank you!" ?></h1>
+
